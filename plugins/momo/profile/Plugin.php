ce608854da9742318bd1f729bda9131c4eb1132f
Unknown <seckndanane16@gmail.com>
2018-07-20 21:56:15 +0100

    Up plugin.php de Profile

--- plugins/momo/profile/Plugin.php
+++ plugins/momo/profile/Plugin.php
@@ -2,13 +2,36 @@
 
 use System\Classes\PluginBase;
 
-class Plugin extends PluginBase
-{
-    public function registerComponents()
-    {
+class Plugin extends PluginBase {
+    public function registerComponents() {
     }
-
-    public function registerSettings()
-    {
+  
+    public function registerSettings() {
     }
-}
+  
+    public function boot() {
+  
+      UserModel::extend(function ($model) {
+        $model->addFillable([
+                              'facebook', 'bio'
+                            ]);
+      });
+  
+      UsersController::extendFormFields(function ($form, $model, $context) {
+        $form->addTabFields([
+                              'facebook' => [
+                                'label' => 'Facebook',
+                                'type'  => 'text',
+                                'tab'   => 'Profile'
+                              ],
+                              'bio'      => [
+                                'label' => 'Biography',
+                                'type'  => 'textarea',
+                                'tab'   => 'Profile'
+                              ],
+  
+                            ]);
+      });
+  
+    }
+  }
